0de73128b517a941e41a18433da06f7ad4a5b31b
Archana O <archana20010622@gmail.com>
2026-07-28 21:52:28 +0530

    Update 02_monthly_cohort_retention.sql

--- queries/02_monthly_cohort_retention.sql
+++ queries/02_monthly_cohort_retention.sql
@@ -1,1 +1,45 @@
+with customer_signup as (
+      select 
+	        customer_id,
+			date_trunc('month',created_at::timestamp)  as cohort_month
+			from ecom.customers
+),
+retention_summary as(
+SELECT
+      cs. cohort_month,
+	  count(distinct cs.customer_id) as cohort_size,
+	  count(
+                distinct 
+				case when date_trunc('month',o.created_at::timestamp) = cs.cohort_month + INTERVAL '1 month' and o.status<>'cancelled' 
+				then cs.customer_id end
+	  )                                                                                                                                    as m1_retention,
+	  count (
+               DISTINCT
+			   case when date_trunc('month',o.created_at::timestamp) =cs.cohort_month+ INTERVAL '2 month' and o.status <>'cancelled' 
+		       then cs.customer_id end
+	  )                                                                                                                                  as m2_retention,
+	  count (
+               DISTINCT
+			   case when date_trunc('month',o.created_at::timestamp) =cs.cohort_month+ INTERVAL '3 month' and o.status <>'cancelled' 
+		       then cs.customer_id end
+	  )                                                                                                                                  as m3_retention
+	  
+	  
 
+
+from customer_signup cs 
+ left join ecom.orders o
+   on cs.customer_id=o.customer_id
+group by cohort_month
+)
+
+SELECT
+      cohort_month,
+	  cohort_size,
+	  m1_retention,
+	  m2_retention,
+	  m3_retention,
+	  m1_retention::numeric/cohort_size   as m1_retention_rate,
+	  m2_retention::numeric/cohort_size   as m2_retention_rate,
+	  m3_retention::numeric/cohort_size   as m3_retention_rate
+from retention_summary
