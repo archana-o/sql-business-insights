3164413b2c73babe3e7ab7a2552364371c1b82fd
Archana O <archana20010622@gmail.com>
2026-08-09 21:48:27 +0530

    Update 01_daily_business_summary.sql

--- queries/01_daily_business_summary.sql
+++ queries/01_daily_business_summary.sql
@@ -4,8 +4,8 @@ select
 	   sum(total)                                                                                                                     as revenue,
 	   count(*)                                                                                                                       as orders,
 	   sum(total)/count(*)                                                                                                            as AOV,
-	   sum(case when payment_status='paid' then 1 else 0 end) *100.00/ count(*)                                                       as paid_order_rate,
-	   sum (case when status='cancelled' then 1 else 0 end) * 100.00/count(*)                                                       as cancelled_order_rate
+	   sum(case when payment_status='paid' then 1 else 0 end) *100.00/ nullif(count(*) ,0)                                                      as paid_order_rate,
+	   sum (case when status='cancelled' then 1 else 0 end) * 100.00/nullif(count(*),0)                                                       as cancelled_order_rate
 from ecom.orders 
 group by date_trunc('day',created_at::timestamp)
 order by date_trunc('day',created_at::timestamp) desc
@@ -28,8 +28,8 @@ select
 	  paid_order_rate,
 	  cancelled_order_rate,
 	  coalesce(refund_amount,0)                                                                                        as refund_amount,
-	  (revenue-lag(revenue) over(order by ordered_date )) * 100.00  / lag(revenue) over(order by ordered_date)          as revenue_vs_yesterday_pct,
-	  (revenue- lag(revenue,7) over(order by ordered_date))*100.00/ lag(revenue,7) over(order by ordered_date)            as revenue_vs_last_weekday_pct       
+	  (revenue-lag(revenue) over(order by ordered_date )) * 100.00  / nullif(lag(revenue) over(order by ordered_date),0)          as revenue_vs_yesterday_pct,
+	  (revenue- lag(revenue,7) over(order by ordered_date))*100.00/ nullif(lag(revenue,7) over(order by ordered_date),0)          as revenue_vs_last_weekday_pct       
 from
 ordersummary left join refundsummary ON
 ordersummary.ordered_date=refundsummary.refund_date
